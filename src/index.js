ae17e488865c963e5dd7ea206ddbebd4450ed48f
south-str <feldweg2@gmail.com>
2019-05-11 02:27:21 +0900

    change to use Promise

--- src/index.js
+++ src/index.js
@@ -1,33 +1,46 @@
+'use strict';
 const fs = require('fs'),
+  util = require('util'),
   http = require('http'),
   https = require('https'),
   http2 = require('http2');
 
-const httpServer = http.createServer((request, response) => {
-  response
-    .writeHead(200, {'Content-Type': 'text/plain'})
-    .end('response');
-});
-httpServer.listen(3000);
-
-const sslOptions = {
-  key: fs.readFileSync(__dirname + '/../.key/localhost-privkey.pem'),
-  cert: fs.readFileSync(__dirname + '/../.key/localhost-cert.pem')
-},
-  httpsServer = https.createServer(sslOptions, (request, response) => {
+(async () => {
+  const httpServer = http.createServer((request, response) => {
     response
       .writeHead(200, {'Content-Type': 'text/plain'})
-      .end('secure response');
+      .end('response');
   });
-httpsServer.listen(3001);
+  httpServer.listen(3000);
 
-const http2Server = http2.createSecureServer(sslOptions);
+  const readFile = util.promisify(fs.readFile),
+    sslOptions = {
+      key: await readFile(__dirname + '/../.key/localhost-privkey.pem')
+      .catch(err => {
+        console.log(err);
+        return err;
+      }),
+      cert: await readFile(__dirname + '/../.key/localhost-cert.pem')
+      .catch(err => {
+        console.log(err);
+        return err;
+      })
+    },
+    httpsServer = https.createServer(sslOptions, (request, response) => {
+      response
+        .writeHead(200, {'Content-Type': 'text/plain'})
+        .end('secure response');
+    });
+  httpsServer.listen(3001);
 
-http2Server.on('stream', (stream, headers) => {
-  stream.respond({
-    'content-type': 'text/plain',
-    ':status': 200
+  const http2Server = http2.createSecureServer(sslOptions);
+
+  http2Server.on('stream', (stream, headers) => {
+    stream.respond({
+      'content-type': 'text/plain',
+      ':status': 200
+    });
+    stream.end('http2 response');
   });
-  stream.end('http2 response');
-});
-http2Server.listen(3002);
+  http2Server.listen(3002);
+})();
